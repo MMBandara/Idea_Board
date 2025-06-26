1c3f7f24223d68aed96067352b366543f2b267a7
MMBandara <152193593+MMBandara@users.noreply.github.com>
2025-06-26 22:29:59 +0530

    Create the model for the idea

--- ideaBoard-backend/src/main/java/com/menura/ideaboardbackend/model/IdeaModel.java
+++ ideaBoard-backend/src/main/java/com/menura/ideaboardbackend/model/IdeaModel.java
@@ -1,5 +1,24 @@
 package com.menura.ideaboardbackend.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
+
+@Entity
 public class IdeaModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NotBlank
+    private String title;
+
+    @NotBlank
+    private String description;
+
+    private LocalDateTime createAt = LocalDateTime.now();
 
 }
